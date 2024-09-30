d85e20c24cda24574cb8ea2b003a10d4db749f4a
Nicolas Fels <nicolas.fels@cpe.fr>
2024-09-30 17:46:22 +0200

    update graphic

--- graphic.py
+++ graphic.py
@@ -4,7 +4,26 @@ Nicolas FELS
 But: Jeu du pendu en version graphique
 A faire:'''
 
-from tkinter import Tk, Label, Button
+import tkinter as tk
 
+class Jeu():
+    def __init__(self):
+        self.dico = {"bjr"}
+        self.chance = 8
+
+
+class Fenetre():
+    def __init__(self, root):
+        self.nom = "Jeu du pendu"
+        self.jeu = Jeu()
+        root.title(self.nom)
+        self.image = [tk.PhotoImage(file = f"bonhomme{i}.gif" for i in range(1,9))]
+        self.canvas = tk.Canvas(root, width = self.imag[0].width(), height = self.image[0].height())
+        self.canvas.pack()
+        self.canvas.create_image(0, 0, anchor = "nw", image = self.image[4])
+        self.input = tk.Entry()
+        self.input.pack()
+        self.bouton = tk.Button(root, txt = "ENTRY", command = Jeu.''(self.iput.get()))
+        self.bouton.pack()
 
 
